75415f16c4606aa973b7366a01835de37cc4645d
Workshop Participant <participant@example.com>
2025-11-12 14:58:59 +0000

    added logistics mcp

--- agents/management_agent.py
+++ agents/management_agent.py
@@ -13,9 +13,10 @@ from strands.models import BedrockModel
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import get_bedrock_model_for_agent
 
+#Your role is to optimize order sequency when a new order arrives in json format and answer with the updated order sequency list in json format. For this you need to do the following steps in this order:
 
-SYSTEM_PROMPT = """You are a management assistant for an t-shirt e-commerce company.
-Your role is to optimize order sequency when a new order arrives and answer with the updated order sequency in json format. For this you need to do the following steps:
+
+SYSTEM_PROMPT = """You are a management assistant for an t-shirt e-commerce company. Your job is to return the json that you have been provided.
 
 """
 
@@ -38,41 +39,31 @@ _mcp_client = None
 async def invoke(payload, context=None):
     """AgentCore Runtime entrypoint function"""
     user_input = payload.get("prompt", "")
-
-    # Access request headers - handle None case
-    request_headers = context.request_headers or {}
-
-    # Get Client JWT token
-    auth_header = request_headers.get('Authorization', '')
-
-    print(f"Authorization header: {auth_header}")
     
-    '''from mcp.client.stdio import stdio_client, StdioServerParameters 
+    from mcp.client.stdio import stdio_client, StdioServerParameters 
     mes_client = MCPClient(lambda: stdio_client(
         StdioServerParameters(
             command="uv",
-            args=["run", "python", "mcp_servers/servers/mes_mcp_server.py", "--stdio"]
+            args=["run", "python", "../mcp_servers/servers/mes_mcp_server.py", "--stdio"]
         )
     ))
     erp_client = MCPClient(lambda: stdio_client(
         StdioServerParameters(
             command="uv",
-            args=["run", "python", "mcp_servers/servers/erp_mcp_server.py", "--stdio"]
+            args=["run", "python", "../mcp_servers/servers/erp_mcp_server.py", "--stdio"]
         )
     ))
     logistic_client = MCPClient(lambda: stdio_client(
         StdioServerParameters(
             command="uv",
-            args=["run", "python", "mcp_servers/servers/logistic_mcp_server.py", "--stdio"]
+            args=["run", "python", "../mcp_servers/servers/logistic_mcp_server.py", "--stdio"]
         )
     ))
-    all_mcp_tools = mes_client.list_tools_sync() + erp_client.list_tools_sync() + logistic_client.list_tools_sync()'''
-
 
     agent = Agent(
         model='us.anthropic.claude-sonnet-4-20250514-v1:0',
         system_prompt=SYSTEM_PROMPT,
-        # tools=[all_mcp_tools]
+        tools=[erp_client, mes_client, logistic_client]
     )
     # Invoke the agent
     response = agent(user_input)
